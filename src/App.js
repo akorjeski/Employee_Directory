e02e67e0c9384852f58d40250e356c9344434316
Alex Korjeski <akorjeski@gmail.com>
2020-04-11 16:13:43 -0400

    initial git push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Jumbotron from "./components/Jumbotron";
+import Table from "./components/Table"; 
+
+function App() {
+  return (
+    <div>
+    <Jumbotron/>
+    <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Jumbotron.css"
+
+function Jumbotron() {
+  return (
+  <div>
+    <div className="jumbotron jumbotron-fluid" id="jumboTron">
+      <div className="container">
+        <h1 className="display-3"><b>Employee Directory</b></h1>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Jumbotron;
